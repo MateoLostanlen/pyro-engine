112a9bd6039326adce4094ddb5e714b1a9ad54e7
Mateo <mateo.lostanlen@gmail.com>
2023-09-01 14:53:04 +0200

    update script

--- scripts/update_script.sh
+++ scripts/update_script.sh
@@ -9,8 +9,10 @@
 # This script must be run with a crontab, run every day at 3am
 # 0 3 * * * bash /home/pi/pyro-engine/scripts/update_script.sh
 
+# Enable debugging
+set -x
 
-if [[ `git -C /home/pi/pyro-engine pull origin auto_update | grep -c "up to date."` == 1 ]];
+if git -C /home/pi/pyro-engine pull origin auto_update | grep -q "Already up to date.";
     then
         echo "pyro-engine up to date";
     else
@@ -21,17 +23,11 @@ if [[ `git -C /home/pi/pyro-engine pull origin auto_update | grep -c "up to date
         docker-compose down
         docker build . -t pyronear/pyro-engine:latest
 	    docker-compose up -d
-        # echo "pyro-engine updated from github";
-        # echo "stop" >> /home/pi/log.txt
-        # date >> /home/pi/log.txt
-        # docker container stop pyro-engine_pyro-engine_1;
-        # echo "build" >> /home/pi/log.txt
-        # date >> /home/pi/log.txt
-        # docker build /home/pi/pyro-engine -t pyronear/pyro-engine:latest;
-        # echo "run" >> /home/pi/log.txt
-        # date >> /home/pi/log.txt
-        # docker-compose -f /home/pi/pyro-engine/docker-compose.yml up -d;
-        # echo "done" >> /home/pi/log.txt
-        # date >> /home/pi/log.txt
 
 fi;
+
+date >> /home/pi/log.txt
+exec >> /home/pi/log.txt 2>&1
+
+# Disable debugging if needed
+set +x
