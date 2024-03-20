9e67ae4dad91758174e162dc010c22c663bf0775
mateo <mateo.lostanlen@gmail.com>
2024-03-20 12:02:00 +0100

    test action

--- scripts/update_and_execute.sh
+++ scripts/update_and_execute.sh
@@ -19,6 +19,8 @@ if [ "$HEADHASH" != "$UPSTREAMHASH" ]
 then
     echo "$(date): New changes detected ! Updating and executing script..."
     git pull origin main
+    mkdir tt
+    echo "toto" >> tt/tt.txt
     echo "$(date): Update done !"
 
 else
