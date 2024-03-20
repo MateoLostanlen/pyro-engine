5418dd518648f359437e6baceace871e75956153
mateo <mateo.lostanlen@gmail.com>
2024-03-20 11:05:00 +0100

    update script

---
+++ scripts/update_and_execute.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Print current date and time
+echo "$(date): Checking for updates"
+
+# Navigate to the repository directory
+cd /home/mateo/Desktop/engine/pyro-engine
+
+# Check for updates and pull
+git fetch origin
+HEADHASH=$(git rev-parse HEAD)
+UPSTREAMHASH=$(git rev-parse main@{upstream})
+
+if [ "$HEADHASH" != "$UPSTREAMHASH" ]
+then
+    echo "$(date): New changes detected ! Updating and executing script..."
+    git pull origin main
+    # Execute the make run command from the Makefile to rebuild and restart Docker containers
+    make run
+    echo "$(date): Update done !"
+
+else
+    echo "$(date): No changes detected"
+fi
